41e70d37d2a9cb4eb86119b2e978ff6d0180d637
Vinicius <viniciuspascoal013@gmail.com>
2025-07-17 08:25:21 -0300

    init

---
+++ src/test/java/com/example/lanches_api/LanchesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lanches_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanchesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
